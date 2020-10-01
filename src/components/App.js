9fefe468ec5fb97356c901a1876eb0f4f7468899
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-01 16:31:49 -0300

    react

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { ReactComponent } from '*.svg';
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Ola</h1>
+            </div>
+        )
+    }
+}
